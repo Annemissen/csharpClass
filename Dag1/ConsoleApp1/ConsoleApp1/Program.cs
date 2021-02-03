79db25e413d35b5261ac390fc86a6754189fd891
Annemie Gravgaard Schulz <eaaagsc@students.eaaa.dk>
2021-02-03 10:37:12 +0100

    gitignore og dag1

---
+++ Dag1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // String intro
+            string firstFriend = "Maria";
+            string secondFriend = "Sage";
+            Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+            Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+            Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+
+            // Trimming string intro
+            string greeting = "      Hello World!       ";
+            Console.WriteLine($"[{greeting}]");
+
+            string trimmedGreeting = greeting.TrimStart();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            trimmedGreeting = greeting.TrimEnd();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            trimmedGreeting = greeting.Trim();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            // String replace - first arg is text to search for, second arg is the replacing text
+            string sayHello = "Hello World!";
+            Console.WriteLine(sayHello);
+            sayHello = sayHello.Replace("Hello", "Greetings");
+            Console.WriteLine(sayHello);
+
+            // String all caps and lower case
+            Console.WriteLine(sayHello.ToLower());
+            Console.WriteLine(sayHello.ToUpper());
+
+            // String contains
+            string songLyrics = "You say goodbye, and I say hello";
+            Console.WriteLine(songLyrics.Contains("goodbye"));
+            Console.WriteLine(songLyrics.Contains("greetings"));
+
+            Console.WriteLine(songLyrics.StartsWith("You"));
+            Console.WriteLine(songLyrics.StartsWith("goodbye"));
+
+            Console.WriteLine(songLyrics.EndsWith("hello"));
+            Console.WriteLine(songLyrics.EndsWith("goodbye"));
+
+
+
+
+
+        }
+    }
+}
---
+++ Dag1/ConsoleAppMax/ConsoleAppMax/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleAppMax
+{
+    class Program
+    {
+        /**
+         *  It should be possible to call the method, Max, with a random number of double-parameter as shown in the code. 
+         *  Complete the declaration of Max
+         */
+        public static double Max(params double[] values)
+        {
+            double result = values[0];
+            foreach (double value in values)
+            {
+                if (value > result)
+                {
+                    result = value;
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            double m1 = Max(28.5, 17.2);
+            double m2 = Max(4.0, 10.8, 34.25, 2.0, 23.6);
+
+            Console.WriteLine(m1 + " " + m2);
+            Console.ReadLine();
+        }
+    }
+}

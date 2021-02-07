78c92d18af15657fe9f2fa374dc4cb4026ca37cc
Annemie Gravgaard Schulz <eaaagsc@students.eaaa.dk>
2021-02-07 21:11:10 +0100

    dag2 øvelse 7 done

--- Dag2/ValAndRefTypeExample/Program.cs
+++ Dag2/ValAndRefTypeExample/Program.cs
@@ -10,7 +10,11 @@ namespace ConsoleApplication3
         static void Main(string[] args)
         {
             int a = 7;
-            IncrementValue(a);
+            IncrementValueRef(ref a);
+            Console.WriteLine(a);
+
+            a = 7;
+            IncrementValueNoRef(a);
             Console.WriteLine(a);
 
             Integer A = new Integer(7);
@@ -25,9 +29,16 @@ namespace ConsoleApplication3
             A.value = A.value + 1;
         }
 
-        private static void IncrementValue(int a)
+        private static void IncrementValueRef(ref int a)
         {
             a = a + 1;
+        }
+
+        private static void IncrementValueNoRef(int a)
+        {
+            a = a + 1;
+            Console.WriteLine("Should be 8. Result is: " + a);
+
         }
     }
 

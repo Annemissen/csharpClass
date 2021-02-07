989b3d11bb15e79707b3da176539eb45217a33fc
Annemie Gravgaard Schulz <eaaagsc@students.eaaa.dk>
2021-02-07 12:27:38 +0100

    dag2 øvelse 4 beregn alder lavet

---
+++ Dag2/ConsoleAppAgeCalculator/ConsoleAppAgeCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleAppAgeCalculator
+{
+    class Program
+    {
+        /*in is used to state that the parameter passed cannot be modified by the method. 
+        * out is used to state that the parameter passed must be modified by the method*/
+        public static void calcAge(in int birthYear, out int age)
+        {
+            int currentYear = DateTime.Now.Year;
+            age = (currentYear - birthYear);
+            Console.WriteLine(age);
+        }
+        static void Main(string[] args)
+        {
+            int year = 1994;
+            int age;
+            calcAge(in year, out age);
+        }
+    }
+}

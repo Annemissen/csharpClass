7d377b9ad59005f04862d9e1b419305924b92aa4
Annemie Gravgaard Schulz <eaaagsc@students.eaaa.dk>
2021-02-07 20:20:36 +0100

    dag2 øvelse 6 om throw error done

---
+++ Dag2/ConsoleAppWithError/ConsoleAppWithError/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleAppWithError
+{
+    class Program
+    {
+        private static void MyMethodWithError(int num = 0)
+        {
+            throw new Exception("MyMethodWithError: Something went wrong-ish");
+        }
+
+        public static void MyNormalMethod(int num = 0)
+        {
+            try
+            {
+                MyMethodWithError();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("The 'try catch' is finished.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            MyNormalMethod();
+        }
+    }
+}
